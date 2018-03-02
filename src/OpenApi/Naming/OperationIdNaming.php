be26945570c0d2fee1c8da703129c91ceef4555d
Guillermo Omar Lopez Lopez <omar@nutriconsultor.io>
2018-03-01 16:27:24 -0800

    fix: removed invalid characters from operation id.

--- src/OpenApi/Naming/OperationIdNaming.php
+++ src/OpenApi/Naming/OperationIdNaming.php
@@ -9,11 +9,19 @@ class OperationIdNaming implements OperationNamingInterface
 {
     public function getFunctionName(Operation $operation): string
     {
-        return Inflector::camelize((string) $operation->getOperation()->getOperationId());
+        return Inflector::camelize($this->getOperationId($operation));
     }
 
     public function getEndpointName(Operation $operation): string
     {
-        return Inflector::classify((string) $operation->getOperation()->getOperationId());
+        return Inflector::classify($this->getOperationId($operation));
+    }
+    
+    private function getOperationId(Operation $operation): string
+    {
+        $operationId = (string) $operation->getOperation()->getOperationId();
+        // Remove invalid characters.
+        $operationId = preg_replace('/[^a-z0-9 ]+/iu', '_', $operationId);
+        return trim($operationId);
     }
 }
